2eefeadddfc6fe615732cfe5d0e186f42de9c8d8
pemrouz <p3pedram@hotmail.com>
2017-12-07 23:37:49 +0000

    feat: allow multi-key sorting

--- test.js
+++ test.js
@@ -18,4 +18,49 @@ describe('za', function() {
     expect(a.sort(za('val'))).to.be.eql([{ val: 'c' }, { val: 'b' }, { val: 'b' }, { val: 'a' }, {}, {}, {}, {}])
   })
 
+  it('should accept fn as key', function() {
+    var a = [
+      { base: 'USD', quote: 'GBP' }
+    , { base: 'CAD', quote: 'GBP' }
+    , { base: 'USD', quote: 'CHF' }
+    , { base: 'CAD', quote: 'CHF' }
+    , { base: 'USD', quote: 'EUR' }
+    , { base: 'CAD', quote: 'EUR' }
+    ]
+
+    expect(a.sort(za(ccy))).to.be.eql([
+      { base: 'USD', quote: 'GBP' }
+    , { base: 'USD', quote: 'EUR' }
+    , { base: 'USD', quote: 'CHF' }
+    , { base: 'CAD', quote: 'GBP' }
+    , { base: 'CAD', quote: 'EUR' }
+    , { base: 'CAD', quote: 'CHF' }
+    ])
+
+    function ccy(d) {
+      return d.base + d.quote
+    }
+  })
+
+  it('should accept multiple keys', function() {
+    var a = [
+      { name: 'B', ip: '1' }
+    , { name: 'C', ip: 'Y' }
+    , { name: 'C', ip: 'X' }
+    , { name: 'A', ip: '1' }
+    , { name: 'A', ip: '2' }
+    , { name: 'B', ip: '2' }
+    ]
+
+    expect(a.sort(za('name', 'ip'))).to.be.eql([
+      { name: 'C', ip: 'Y' }
+    , { name: 'C', ip: 'X' }
+    , { name: 'B', ip: '2' }
+    , { name: 'B', ip: '1' }
+    , { name: 'A', ip: '2' }
+    , { name: 'A', ip: '1' }
+    ])
+  })
+
+
 })
